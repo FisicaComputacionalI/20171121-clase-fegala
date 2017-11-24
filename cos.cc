a0ff26a6b7dd2364dee7f76272fed1df1eed0847
fegala <31254488+fegala@users.noreply.github.com>
2017-11-24 11:32:46 -0600

    Add files via upload

--- cos.cc
+++ cos.cc
@@ -1,10 +1,21 @@
+
+//Fernando Garza Landa
 #include <iostream>
 #include <math.h>
 using namespace std;
 int main(){
-  float p=0.15;
-  float c=cos(p);
+  float x=0.15;
+  float h;
+  float c=cos(x);
 
-    cout<<"Coseno de 0.15 es "<<c<<endl;
+    cout<<"Coseno de 0.15 es "<<x<<endl;
+    h=.001;
+    c=cos(x+h);
+    cout<<"cos(x+h)="<<c<<endl;
+    c=cos(x+2*h);
+    cout<<"cos(x+2h)="<<c<<endl;
+    
+    cout<<"El valor de h mas pequenio para el cual cos(x+h) y cos(x+2h) aun se note la diferencia hasta milesimas es "<<h<<endl;
+    
   return 0;
 }
